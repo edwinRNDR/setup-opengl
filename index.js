90b8d249dd54b510e8c8936847026daa07cde0a5
Edwin Jakobs <edwin@rndr.studio>
2020-04-26 15:34:28 +0200

    Fix js

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const core = require('@actions/core')
 const {exec} = require('@actions/exec')
 
 try {
-    exec('pwd')
-
     var options = {
         env: {
             'PAYLOAD_PATH' : __dirname
         }
     }
-
     exec(path.join(__dirname, "setup-opengl.sh"))
 } catch(error) {
     core.setFailed(error.message)
